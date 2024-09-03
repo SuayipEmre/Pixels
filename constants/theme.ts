ec91f407db8b9cc0f5c2ea2b6565f21ffde4065e
Şuayip Emre <suayipemredev@gmail.com>
2024-09-04 00:03:15 +0300

    installations, setup theme and helpers

---
+++ constants/theme.ts
@@ -0,0 +1,21 @@
+
+export const THEME = {
+    colors:{
+        white : '#fff',
+        black : '#000',
+        grayBg : '#e5e5e5',
+        neutral : (opacity : number) => `rgba(10, 10, 10, ${opacity})`
+    },
+    fontWeights:{
+        medium : '500',
+        semobold : '600',
+        bold :'700'
+    },
+    radius:{
+        xs:10,
+        sm:12,
+        md:14,
+        lg:16,
+        xl:18
+    },
+} 
